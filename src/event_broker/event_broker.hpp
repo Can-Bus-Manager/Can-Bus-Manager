ccd1f5687b4e62a278e24c7c92327c0ddca2d856
BuildTools <unconfigured@null.spigotmc.org>
2025-12-20 21:47:03 +0100

    CBS-64 fix: pipline error

--- src/event_broker/event_broker.hpp
+++ src/event_broker/event_broker.hpp
@@ -8,24 +8,30 @@
 #include <entt/entt.hpp>
 
 /** TEST */
-class EventBroker {
-public:
-    template<typename T>
-    void trigger(const T& event) {
+class EventBroker
+{
+   public:
+    template <typename T>
+    void trigger(const T& event)
+    {
         dispatcher.trigger(event);
     }
 
-    template<typename T>
-    void enqueue(const T& event) {
+    template <typename T>
+    void enqueue(const T& event)
+    {
         dispatcher.enqueue(event);
     }
 
     void update();
 
-    entt::dispatcher& getDispatcher() { return dispatcher; }
+    entt::dispatcher& getDispatcher()
+    {
+        return dispatcher;
+    }
 
-private:
+   private:
     entt::dispatcher dispatcher;
 };
 
-#endif //CANBUSSIMULATOR_EVENTBROKER_HPP
+#endif  // CANBUSSIMULATOR_EVENTBROKER_HPP
