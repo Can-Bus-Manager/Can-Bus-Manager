4073cfd725fdceceee1b866ac42ba009755663d3
Florian <florian@fehrle.net>
2025-12-29 00:39:26 +0100

    CBS-58 feat: added can communication handler component classes

---
+++ src/can_handler/can_communication_handler/i_can_parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by flori on 28.12.2025.
+//
+
+#include "i_can_parser.hpp"
---
+++ src/core/event/dbc_event.hpp
@@ -0,0 +1,18 @@
+//
+// Created by flori on 29.12.2025.
+//
+
+#ifndef CANBUSMANAGER_DBC_EVENT_HPP
+#define CANBUSMANAGER_DBC_EVENT_HPP
+
+#include "event.hpp"#include "event.hpp"
+namespace Core {
+/**
+ * @brief This structure contains the event for a new DBC structure to be used for de-/encoding
+ */
+struct NewDbcEvent : Event
+{
+
+};
+}
+#endif //CANBUSMANAGER_DBC_EVENT_HPP
