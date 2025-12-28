99d56570b82a3dd0aee7a9bac7563af1f47be9b6
Florian <florian@fehrle.net>
2025-12-28 23:32:18 +0100

    CBS-58 feat: added event broker

--- src/event_broker/event_broker.hpp
+++ src/event_broker/event_broker.hpp
@@ -7,31 +7,39 @@
 
 #include <entt/entt.hpp>
 
-/** TEST */
-class EventBroker
+#include "core/interface/i_event_broker.hpp"
+namespace EventBroker {
+class EventBroker final : Core::IEventBroker
 {
-   public:
-    template <typename T>
-    void trigger(const T& event)
-    {
-        dispatcher.trigger(event);
+protected:
+    void _publish(std::type_index type, const void* data) override;
+    auto _subscribe(std::type_index type, std::function<void(const void*)> callback)
+        -> Core::Connection override;
+
+private:
+    struct Channel {
+        entt::dispatcher dispatcher;
+
+        void trigger(const void* event) {
+            dispatcher.trigger(event);
+        }
+
+        auto sink() {
+            return dispatcher.sink<const void*>();
+        }
+    };
+
+    auto getChannel(const std::type_index type) -> Channel& {
+        auto& ptr = channels[type];
+        if (!ptr) {
+            ptr = std::make_unique<Channel>();
+        }
+        return *ptr;
     }
 
-    template <typename T>
-    void enqueue(const T& event)
-    {
-        dispatcher.enqueue(event);
-    }
-
-    void update();
-
-    auto getDispatcher() -> entt::dispatcher&
-    {
-        return dispatcher;
-    }
-
-   private:
-    entt::dispatcher dispatcher;
+    std::unordered_map<std::type_index, std::unique_ptr<Channel>> channels;
 };
+}
+
 
 #endif  // CANBUSSIMULATOR_EVENTBROKER_HPP
