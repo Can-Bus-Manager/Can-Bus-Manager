4f0106e6fe31f8d15ae42e3b579efc24d2543e81
nelespatzier <nele.spatzier@gmail.com>
2026-01-02 12:52:14 +0100

    Added namespaces to all .hpp files, removed .cpp files from git.

--- src/sending/delegate/SendingDelegate.hpp
+++ src/sending/delegate/SendingDelegate.hpp
@@ -1,27 +1,29 @@
 #ifndef SENDING_DELEGATE_HPP
 #define SENDING_DELEGATE_HPP
-#include <QStyledItemDelegate>
-#include <QWidget>
 #include <QAbstractItemModel>
 #include <QModelIndex>
 #include <QStyleOptionViewItem>
+#include <QStyledItemDelegate>
+#include <QWidget>
+
 #include "core/interface/i_event_broker.hpp"
 using Core::IEventBroker;
 
 namespace Sending {
-class SendingDelegate : public QStyledItemDelegate {
+class SendingDelegate : public QStyledItemDelegate
+{
     Q_OBJECT
-public:
-    explicit SendingDelegate(IEventBroker* eventBroker, QObject* parent = nullptr);
+   public:
+    explicit SendingDelegate(IEventBroker *eventBroker, QObject *parent = nullptr);
     void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index);
-    QWidget* createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
+                          const QModelIndex &index) const override;
     void setEditorData(QWidget *editor, const QModelIndex &index) const override;
-    void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
-    void paint(QPainter *painter,
-               const QStyleOptionViewItem &option,
+    void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option,
+                              const QModelIndex &index) const override;
+    void paint(QPainter *painter, const QStyleOptionViewItem &option,
                const QModelIndex &index) const override;
-    QSize sizeHint(const QStyleOptionViewItem &option,
-                     const QModelIndex &index) const override;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 };
-}
-#endif // SENDING_DELEGATE_HPP
+}  // namespace Sending
+#endif  // SENDING_DELEGATE_HPP
