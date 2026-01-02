4f40a514bcb944fb9659e6d09f9eff9cda697abc
nelespatzier <nele.spatzier@gmail.com>
2026-01-02 12:39:05 +0100

    Removed newline

--- src/sending/view/SendingView.hpp
+++ src/sending/view/SendingView.hpp
@@ -2,28 +2,30 @@
 #ifndef SENDINGVIEW_HPP
 #define SENDINGVIEW_HPP
 #include <QWidget>
+
 #include "core/interface/i_event_broker.hpp"
-#include "sending/model/SendingModel.hpp"
 #include "sending/delegate/SendingDelegate.hpp"
+#include "sending/model/SendingModel.hpp"
 
 namespace Sending {
 
-class SendingView : public QWidget {
+class SendingView : public QWidget
+{
     Q_OBJECT
 
-public:
-    explicit SendingView(IEventBroker* broker, QWidget *parent = nullptr);
+   public:
+    explicit SendingView(IEventBroker *broker, QWidget *parent = nullptr);
     ~SendingView();
 
-private slots:
+   private slots:
     // Slots to handle button clicks from the UI
     void on_btnSendMessage_clicked();
     void on_btnToggleCyclic_clicked();
-    
-private:
+
+   private:
     Sending::SendingView *ui;
-    SendingModel *m_model;        //data source
-    SendingDelegate *m_delegate;  //custom editor
+    SendingModel *m_model;        // data source
+    SendingDelegate *m_delegate;  // custom editor
 };
-}
-#endif // SENDINGVIEW_HPP
+}  // namespace Sending
+#endif  // SENDINGVIEW_HPP
